5a5b9761bf2d2cbbfc9fd513d13df1db45697b35
wellington pecchini <wellington@MBPdiwelington2.wind3.hub>
2024-05-03 10:10:53 +0200

    remove pages

--- src/app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "../sections/hero/hero";
-
-export default function Home() {
-  return (
-    <main className="home">
-      <Hero />
-    </main>
-  );
-}
--- src/app/sections/hero/hero.tsx
+++ src/app/sections/hero/hero.tsx
@@ -1,23 +1,14 @@
-import Image from "next/image";
 import React from "react";
 import MG from "/public/img/m&g.jpeg";
 import AttachmentBg from "@/app/components/AttachmentBg/AttachmentBg";
-import Button from "@/app/components/Buttons/button";
-import { BUTTON_SIZES } from "@/app/components/Buttons/enums";
-import RingsSvg from "@/app/components/Svgs/Rings";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="message">
         <h1 className="solution">Mary & Giovi</h1>
-        {/*  <RingsSvg /> */}
-        {/* <p className="slogan">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p> */}
       </div>
       <AttachmentBg className="hero__bg" bg={MG} />
-      {/*  <Image className="hero__bg" alt="programmer" src={Programmer} /> */}
     </div>
   );
 };

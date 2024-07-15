0108a66fbb03a43c63ffd99f5d5dd71c8b8819eb
wellington pecchini <wellington@MBPdiwelington2.homenet.telecomitalia.it>
2024-07-15 19:45:03 +0200

    f

--- src/app/utils/routes.ts
+++ src/app/utils/routes.ts
@@ -1,12 +1,10 @@
 export const Links: any[] = [
   { route: "/", name: "Home" },
-  { route: "/visitPlace", name: "Visita Valencia" },
-  { route: "/visitRestaurant", name: "Mangia a Valencia" },
   { route: "/place", name: "Location" },
   { route: "/receiving", name: "Ricevimento" },
+  { route: "/booking", name: "Dove dormire" },
+  { route: "/visitPlace", name: "Visita Valencia" },
+  { route: "/visitRestaurant", name: "Mangia a Valencia" },
+  { route: "/moreInfo", name: "Altro" },
   { route: "/gift", name: "Regalo" },
-  /*   { route: "/ilgrandegiorno", name: "Il Grande giorno" },
-  { route: "/presenza", name: "Ci sarai?" },
-  { route: "/cosafare", name: "Cosa da fare" },
-  { route: "/contatti", name: "Contattaci" }, */
 ];

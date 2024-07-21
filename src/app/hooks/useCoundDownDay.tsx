d93253ff332d4637a98fa85e5942cebb22043040
wellington pecchini <wellington@MBPdiwelington2.homenet.telecomitalia.it>
2024-07-21 08:24:19 +0200

    fix use count

--- src/app/hooks/useCoundDownDay.tsx
+++ src/app/hooks/useCoundDownDay.tsx
@@ -1,22 +1,20 @@
 import { useState, useEffect, useCallback } from "react";
 
 const useCountdownDay = (targetDate: any) => {
-  console.log("TARGET", targetDate);
-
   const date = new Date(targetDate);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
+  const day = String(date.getDate()).padStart(2, "0") as any;
+  const month = String(date.getMonth() + 1).padStart(2, "0") as any;
+  const year = date.getFullYear() as any;
 
   const calculateDaysLeft = useCallback(() => {
-    const currentDate = new Date();
+    const currentDate = new Date() as any;
     /*  const [day, month, year] = targetDate.split("/").map(Number); */
-    const target = new Date(year, month - 1, day);
+    const target = new Date(year, month - 1, day) as any;
 
     const timeDiff = target - currentDate;
     const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
     return daysLeft;
-  }, [targetDate]);
+  }, [day, month, year]);
 
   const [daysLeft, setDaysLeft] = useState(calculateDaysLeft());
 

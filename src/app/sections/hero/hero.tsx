ba3dc1ce9465aed3af12dac062e9da3d5d30051e
wellington pecchini <wellington@MBPdiwelington2.homenet.telecomitalia.it>
2024-07-21 09:04:40 +0200

    add hero

--- src/app/sections/hero/hero.tsx
+++ src/app/sections/hero/hero.tsx
@@ -1,24 +1,17 @@
 "use client";
 import React from "react";
-import MG from "/public/img/m&g2.png";
+import MG from "/public/img/m&g1.jpg";
 import AttachmentBg from "@/app/components/AttachmentBg/AttachmentBg";
-import Image from "next/image";
-import Girasoli from "@/../public/img/gira.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="message">
         {/*  <h1 className="solution">Mary & Giovi</h1> */}
-        <h1 className="solution">
-          <span className="solution__names">Maria</span>
-          <Image
-            className="solution__names-gira"
-            src={Girasoli}
-            alt="girasoli"
-          />
-          <span className="solution__names">Giovanni</span>
-        </h1>
+        <div className="solution">
+          <div className="date">02 - 11 - 2024</div>
+          <h1 className="name">Maria e Giovanni</h1>
+        </div>
       </div>
       <AttachmentBg className="hero__bg" bg={MG} />
     </div>

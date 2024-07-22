a64e39b3e21b0857c9bfbb57c380bcd2c7707d7b
wellington pecchini <wellington@MBPdiwelington2.homenet.telecomitalia.it>
2024-07-22 11:23:51 +0200

    add new form

--- src/app/(routes)/participant/page.tsx
+++ src/app/(routes)/participant/page.tsx
@@ -5,9 +5,9 @@ const Participant = () => {
     <div className="participant">
       <iframe
         className="participant__form"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdzxwrKe5Czkg_JzhspURHVo7xNPOOHe3p09TTuhVCPzfgdoA/viewform?embedded=true"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeZdYMUfnWi5WZk7Io6nDjsVDa9FD-W0FGJXPJMI8k_vvUMlg/viewform?embedded=true"
         width="640"
-        height="2441"
+        height="2603"
       >
         Caricamento…
       </iframe>
